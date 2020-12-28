46caf1b05882187b13226fd11ee69adb1b299b3a
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2020-12-28 16:13:03 +0400

    Product details reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,19 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import {productListReducer} from "./reducers/productReducers"
+import {
+	productListReducer,
+	productDetailsReducer,
+} from "./reducers/productReducers";
 
 const reducer = combineReducers({
-	productList: productListReducer
+	productList: productListReducer,
+	productDetails: productDetailsReducer,
 });
 
 const initialState = {};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
 const store = createStore(
 	reducer,
