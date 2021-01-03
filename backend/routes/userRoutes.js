dfc2dcffaaf5391a096e664990660c161f1b9505
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2021-01-03 16:19:02 +0400

    user reg

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, registerUser, getUserProfile } from "../controllers/userController.js";
 const router = express.Router();
+import {protect} from "../Middleware/authMiddleware.js"
 
+router.route("/").post(registerUser)
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile)
 
 export default router;
