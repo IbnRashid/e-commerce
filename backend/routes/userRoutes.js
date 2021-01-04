8baa5ac7ecf76e20822fd064058a0485563063f3
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2021-01-04 15:19:37 +0400

    updated profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { authUser, registerUser, getUserProfile } from "../controllers/userController.js";
+import {
+	authUser,
+	registerUser,
+	getUserProfile,
+	updateUserProfile,
+} from "../controllers/userController.js";
 const router = express.Router();
-import {protect} from "../Middleware/authMiddleware.js"
+import { protect } from "../Middleware/authMiddleware.js";
 
-router.route("/").post(registerUser)
+router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile)
+router
+	.route("/profile")
+	.get(protect, getUserProfile)
+	.put(protect, updateUserProfile);
 
 export default router;
