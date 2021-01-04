03b98573d230de66d59736b0e0e3a404376d495a
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2021-01-04 16:36:06 +0400

    Profile screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import {
-    USER_LOGIN_FAIL,
+	USER_DETAILS_FAIL,
+	USER_DETAILS_REQUEST,
+	USER_DETAILS_SUCCESS,
+	USER_LOGIN_FAIL,
 	USER_LOGIN_REQUEST,
 	USER_LOGIN_SUCCESS,
 	USER_LOGOUT,
@@ -30,24 +33,24 @@ export const login = (email, password) => async (dispatch) => {
 		dispatch({
 			type: USER_LOGIN_SUCCESS,
 			payload: data,
-        });
-        
-        localStorage.setItem("userInfo", JSON.stringify(data))
+		});
+
+		localStorage.setItem("userInfo", JSON.stringify(data));
 	} catch (error) {
-        dispatch({
+		dispatch({
 			type: USER_LOGIN_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
 					: error.message,
 		});
-    }
+	}
 };
 
 export const logout = () => (dispatch) => {
-	localStorage.removeItem("userInfo")
-	dispatch({type: USER_LOGOUT})
-}
+	localStorage.removeItem("userInfo");
+	dispatch({ type: USER_LOGOUT });
+};
 
 export const register = (name, email, password) => async (dispatch) => {
 	try {
@@ -71,20 +74,54 @@ export const register = (name, email, password) => async (dispatch) => {
 			type: USER_REGISTER_SUCCESS,
 			payload: data,
 		});
-		
+
 		dispatch({
 			type: USER_LOGIN_SUCCESS,
 			payload: data,
-        });
-        
-        localStorage.setItem("userInfo", JSON.stringify(data))
+		});
+
+		localStorage.setItem("userInfo", JSON.stringify(data));
 	} catch (error) {
-        dispatch({
+		dispatch({
 			type: USER_REGISTER_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
 					: error.message,
 		});
-    }
+	}
+};
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: USER_DETAILS_REQUEST,
+		});
+
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		const { data } = await axios.get(`/api/users/${id}`, config);
+
+		dispatch({
+			type: USER_DETAILS_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: USER_DETAILS_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
 };
