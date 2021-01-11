a28f44717a0376bd0b5c2ae1f0448e2f1b51285a
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2021-01-11 16:30:17 +0400

    fixed bug in order reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -9,32 +9,6 @@ import {
 
 export const orderCreateReducer = (state = {}, action) => {
 	switch (action.type) {
-		case ORDER_DETAILS_REQUEST:
-			return {
-				...state,
-				loading: true,
-			};
-
-		case ORDER_DETAILS_SUCCESS:
-			return {
-				loading: false,
-				order: action.payload,
-			};
-		case ORDER_DETAILS_FAIL:
-			return {
-				loading: false,
-				error: action.payload,
-			};
-		default:
-			return state;
-	}
-};
-
-export const orderDetailsReducer = (
-	state = { orderItems: [], shippingAddress: {} },
-	action
-) => {
-	switch (action.type) {
 		case ORDER_CREATE_REQUEST:
 			return {
 				loading: true,
@@ -47,6 +21,32 @@ export const orderDetailsReducer = (
 				order: action.payload,
 			};
 		case ORDER_CREATE_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
+
+export const orderDetailsReducer = (
+	state = { loading: true, orderItems: [], shippingAddress: {} },
+	action
+) => {
+	switch (action.type) {
+		case ORDER_DETAILS_REQUEST:
+			return {
+				...state,
+				loading: true,
+			};
+
+		case ORDER_DETAILS_SUCCESS:
+			return {
+				loading: false,
+				order: action.payload,
+			};
+		case ORDER_DETAILS_FAIL:
 			return {
 				loading: false,
 				error: action.payload,
