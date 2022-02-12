eec8c3e09f27a72f03e5ed646326475fec73a72c
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2022-02-12 17:02:48 +0400

    admin user list

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,15 +2,14 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
-import {logout} from "../actions/userActions"
-
+import { logout } from "../actions/userActions";
 
 const Header = () => {
-	const dispatch = useDispatch()
+	const dispatch = useDispatch();
 	const userLogin = useSelector((state) => state.userLogin);
 	const { userInfo } = userLogin;
 	const logoutHandler = () => {
-		dispatch(logout())
+		dispatch(logout());
 	};
 	return (
 		<header>
@@ -45,6 +44,19 @@ const Header = () => {
 										<i className="fas fa-user"></i>Sign In
 									</Nav.Link>
 								</LinkContainer>
+							)}
+							{userInfo && userInfo.isAdmin && (
+								<NavDropdown title="Admin" id="adminmenu">
+									<LinkContainer to="/admin/userList">
+										<NavDropdown.Item>Users</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to="/admin/productList">
+										<NavDropdown.Item>Products</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to="/admin/orderList">
+										<NavDropdown.Item>Orders</NavDropdown.Item>
+									</LinkContainer>
+								</NavDropdown>
 							)}
 						</Nav>
 					</Navbar.Collapse>
