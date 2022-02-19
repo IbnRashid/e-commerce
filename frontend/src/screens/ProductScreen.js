7302bfe7f6115b15bfc3bcf0a3eaa671402da948
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2022-02-19 23:52:33 +0400

    product review on product review screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -37,11 +37,27 @@ const ProductScreen = ({ history, match }) => {
 		productReviewCreate;
 
 	useEffect(() => {
+		if (successProductReview) {
+			alert("Review Submitted!");
+			setRating(0);
+			setComment("");
+			dispatch({ type: PRODUCT_CREATE_REVIEW_RESET });
+		}
 		dispatch(listProductDetails(match.params.id));
-	}, [dispatch, match]);
+	}, [dispatch, match, successProductReview]);
 
 	const addToCartHandler = () => {
 		history.push(`/cart/${match.params.id}?qty=${qty}`);
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		dispatch(
+			createProductReview(match.params.id, {
+				rating,
+				comment,
+			})
+		);
 	};
 
 	return (
