d1a7099a07d177ccd69b81f56b47ac20607551cc
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2022-02-19 23:29:41 +0400

    customer product review submission

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
 	productDeleteReducer,
 	productCreateReducer,
 	productUpdateReducer,
+	productReviewCreateReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -33,6 +34,7 @@ const reducer = combineReducers({
 	productDelete: productDeleteReducer,
 	productCreate: productCreateReducer,
 	productUpdate: productUpdateReducer,
+	productReviewCreate: productCreateReducer,
 	cart: cartReducer,
 	userLogin: userLoginReducer,
 	userRegister: userRegisterReducer,
@@ -46,7 +48,7 @@ const reducer = combineReducers({
 	orderPay: orderPayReducer,
 	orderDeliver: orderDeliverReducer,
 	orderListMy: orderListMyReducer,
-	orderList: orderListReducer
+	orderList: orderListReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
