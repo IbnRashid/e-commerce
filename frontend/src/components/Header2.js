7539d1b308dc9d71b850aaa1717cd3c5604fa597
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2022-03-04 19:58:27 +0400

    secondary test navbar

--- frontend/src/components/Header2.js
+++ frontend/src/components/Header2.js
@@ -2,13 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import img from "../computech-logo.svg";
 
-
 const Header2 = () => {
 	return (
 		<header className="test-header">
-			<Link to="/">
-				<img className="computech-logo" src={img} alt="#" />
-			</Link>
+			<div>
+				<Link to="/">
+					<img className="computech-logo" src={img} alt="#" />
+				</Link>
+			</div>
+			<div>
+				<Link to="/cart">
+					<i className="fas fa-shopping-cart"></i> Cart
+				</Link>
+				<Link to="/login">
+					<i className="fas fa-user"></i> Sign In
+				</Link>
+			</div>
 		</header>
 	);
 };
