fd65271ca469b069bb5295f60c5bc3ef387778bc
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2022-09-07 15:40:53 +0400

    completed navbar hamburger menu

--- frontend/src/components/Header2.js
+++ frontend/src/components/Header2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import img from "../computech-logo.svg";
 
@@ -12,27 +12,51 @@ const Header2 = () => {
 	// 	dispatch(logout());
 	// };
 
+	const primaryNav = document.querySelector(".primary-navigation");
+	const navToggle = document.querySelector(".mobile-nav-toggle");
+	// const visibility = primaryNav.getAttribute("data-visible")
+	// console.log(visibility);
+	const [toggle, setToggle] = useState(false)
+	
+
 	return (
 		<header className="test-header">
-			<div className="nav-left">
-				<Link to="/">
-					<img className="computech-logo" src={img} alt="#" />
-				</Link>
-
+			<Link to="/">
+				<img className="computech-logo" src={img} alt="#" />
+			</Link>
+			<button
+				className="mobile-nav-toggle"
+				aria-controls="primary-navigation"
+				aria-expanded="false"
+				onClick={() => setToggle(!toggle)}
+			>
+				<span className="sr-only"></span>
+			</button>
+			<div
+				id="primary-navigation"
+				data-visible={toggle ? "true" : "false"}
+				className="nav-collapse primary-navigation"
+			>
 				<form className="search-bar">
-					<input name="q" placeholder="Search Products..." type="text" className="search-input" />
-					<button type="submit" className="search-btn">Search</button>
+					<input
+						name="q"
+						placeholder="Search Products..."
+						type="text"
+						className="search-input"
+					/>
+					<button type="submit" className="search-btn">
+						Search
+					</button>
 				</form>
 
-				
-			</div>
-			<div className="nav-right">
-				<Link to="/cart" className="nav-link">
-					<i className="fas fa-shopping-cart"></i> Cart
-				</Link>
-				<Link to="/login" className="nav-link">
-					<i className="fas fa-user"></i> Sign In
-				</Link>
+				<div className="nav-right">
+					<Link to="/cart" className="nav-link">
+						<i className="fas fa-shopping-cart"></i> Cart
+					</Link>
+					<Link to="/login" className="nav-link">
+						<i className="fas fa-user"></i> Sign In
+					</Link>
+				</div>
 			</div>
 		</header>
 	);
